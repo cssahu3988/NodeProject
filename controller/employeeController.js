962b662de4b1c9c08ce0dd098f27728f2bafadc8
Chandra Sekhar Sahu <74488706+cssahu3988@users.noreply.github.com>
2022-01-20 16:33:19 +0530

    c2

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,8 +1,22 @@
+var Joi = require("joi");
 const express = require('express');
 var router = express.Router();
+
+let ar = [{id:1,name:"entry1"},{id:2,name:"entry2"},{id:3,name:"entry3"},{id:4,name:"entry4"}]
+
+const schema = {name:Joi.string().min(3).required()};
+
+
 
 router.get('/',(req,res)=>{
     res.json("this is the response from server.");
 })
+
+router.post('/',(req, res)=>{
+
+    const entry = {id:ar.length+1,name:req.body.name};
+    ar.push(entry);
+    res.send(entry);
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -7,4 +7,6 @@ app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
 });
 
+app.use(express.json());
+
 app.use('/employee', employeeController);
