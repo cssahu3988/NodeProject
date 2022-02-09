4c28bc0f68791cca51439f4ad656b41d5e464d51
Chandra Sekhar Sahu <cssahu3988@gmail.com>
2022-02-09 05:09:35 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const app = express();
 const employeeController = require('./controller/employeeController');
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("Server is running on port 3000")
 });
 
