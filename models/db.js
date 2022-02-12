6d0f3e8a46d9bfa2c4338cfbe06c55f68655d2cd
Chandra Sekhar Sahu <cssahu3988@gmail.com>
2022-02-12 14:02:39 +0530

    c5

--- models/db.js
+++ models/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 mongoose.connect('mongodb://localhost:27017/Employees',{useNewUrlParser:true},(err)=>{
     if(!err){console.log("DB successfully created . ")}
     else{console.log("DB error "+err)}
